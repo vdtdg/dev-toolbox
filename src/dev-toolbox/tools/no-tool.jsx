d8a520d6e11155c0a2b5a7c445c284b3aad0c234
vdtdg <valerian@dtdg.fr>
2023-12-26 18:49:04 +0100

    Added more text to present the tool.

--- src/dev-toolbox/tools/no-tool.jsx
+++ src/dev-toolbox/tools/no-tool.jsx
@@ -4,6 +4,21 @@ export default function NoTool() {
     return <>
         <h3>Welcome to the dev toolbox ! 👋 </h3>
         <p>
+            Welcome to Dev Toolbox, a unique developer's companion for all your coding needs. This toolbox is designed to be simple, efficient, and
+            respectful of your privacy. It's fully client-side, meaning all processing is done in your browser without the need for server-side
+            interaction.
+            <br/>
+            <br/>
+
+            <b>Features:</b>
+            <ul>
+                <li>Lightweight: Optimized for speed and efficiency.</li>
+                <li>No Ads or cookies: Enjoy an uninterrupted experience without tracking.</li>
+                <li>Fully Client-Side: Your code and data stay on your machine, ensuring privacy and security.</li>
+                <li>Versatile Tools: Includes a variety of tools useful for developers regardless of different languages and frameworks.</li>
+            </ul>
+            <br/>
+            <br/>
             To start using the toolbox, select a tool on the left hand side panel.
             <br/>
             👈
