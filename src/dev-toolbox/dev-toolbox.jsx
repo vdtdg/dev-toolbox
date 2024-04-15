1737cdd27d8f4891514da7bc67f278e8582b808a
Saif Ali <116622210+saiksaif@users.noreply.github.com>
2024-04-15 13:04:42 +0500

    [ADD] Search, Categories, Responsiveness to Tools List (#2)
    Implementation of new features:
    1- Added Search bar over tools list to search for tools.
    2- Divided tools into categories in accordions.
    3- Added responsiveness. to tools list and added them to hamburger menu in mobile.

--- src/dev-toolbox/dev-toolbox.jsx
+++ src/dev-toolbox/dev-toolbox.jsx
@@ -1,41 +1,122 @@
-import {NavLink, Outlet, useLocation} from 'react-router-dom';
-import React from 'react';
-import "./dev-toolbox.css"
+import { NavLink, Outlet, useLocation } from "react-router-dom";
+import React from "react";
+import "./dev-toolbox.css";
 
 import NoTool from "./tools/no-tool";
-import {toolList} from "./tool-list";
-
+import { toolList } from "./tool-list";
+import { slide as Menu } from "react-burger-menu";
+import { useState, useEffect } from "react";
 
 export default function DevToolBox() {
-    const currentLocation = useLocation().pathname
-    const noToolLoaded = currentLocation === "/" || currentLocation === "/"
-    const tool = noToolLoaded ? <NoTool/> : <Outlet/>
+  const currentLocation = useLocation().pathname;
+  const noToolLoaded = currentLocation === "/" || currentLocation === "/";
+  const tool = noToolLoaded ? <NoTool /> : <Outlet />;
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
+  const categories = [...new Set(toolList.map((tool) => tool.category))];
+  const [searchTerm, setSearchTerm] = useState("");
 
-    return <>
-        <div className="main-container">
-            <div className="toolbox-container">
-                <aside className="tool-list">
-                    <ul>
-                        <li key="tool-list-title" className="tool-list-title">Available tools ({toolList.length})</li>
-                        {toolList.map((tool) =>
-                            <li key={tool.path}>
-                                <NavLink to={`/${tool.path}`} className="tool-link">
-                                    {tool.name}
-                                </NavLink>
-                            </li>)
-                        }
+  const filteredTools = toolList.filter((tool) =>
+    tool.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 600);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="main-container">
+        <div className="toolbox-container">
+          {isMobile ? (
+            <Menu
+              isOpen={isOpen}
+              onStateChange={({ isOpen }) => setIsOpen(isOpen)}
+            >
+              <aside className="tool-list">
+                <li key="tool-list-title" className="tool-list-title">
+                  Available tools ({filteredTools.length})
+                </li>
+                <div className="searchContain">
+                  <input
+                    className="search-bar"
+                    type="search"
+                    placeholder="Search ..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                  />
+                </div>
+                {categories.map((category) => (
+                  <details key={category}>
+                    <summary className="category-title">{category} - {category.length}</summary>
+                    <ul className="links-ul">
+                      {filteredTools
+                        .filter((tool) => tool.category === category)
+                        .map((tool) => (
+                          <li key={tool.path}>
+                            <NavLink to={`/${tool.path}`} className="tool-link">
+                              {tool.name}
+                            </NavLink>
+                          </li>
+                        ))}
                     </ul>
-                </aside>
-                <div className="tool-container">
-                    {tool}
-                </div>
-            </div>
-            <footer className="toolbox-footer">
-                <div className="toolbox-footer-text">All tools are running locally in your browser: no data is sent to the server &nbsp;|&nbsp;
-                    Made by <a href="https://valerian.dtdg.fr">Valerian de Thézan de Gaussan</a> &nbsp;|&nbsp;
-                    Code is open-sourced <a href="https://github.com/vdtdg/dev-toolbox">here</a>, contributions are welcomed!
-                </div>
-            </footer>
+                  </details>
+                ))}
+              </aside>
+            </Menu>
+          ) : (
+            <aside className="tool-list">
+              <li key="tool-list-title" className="tool-list-title">
+                Available tools{" "}
+                <span className="tools-length">({filteredTools.length})</span>
+              </li>
+              <div className="searchContain">
+                <input
+                  className="search-bar"
+                  type="search"
+                  placeholder="Search tools..."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+              </div>
+              {categories.map((category) => (
+                <details key={category} className="category-section" open>
+                  <summary className="category-title">{category}</summary>
+                  <ul className="links-ul">
+                    {filteredTools
+                      .filter((tool) => tool.category === category)
+                      .map((tool) => (
+                        <li key={tool.path}>
+                          <NavLink to={`/${tool.path}`} className="tool-link">
+                            {tool.name}
+                          </NavLink>
+                        </li>
+                      ))}
+                  </ul>
+                  <br />
+                </details>
+              ))}
+            </aside>
+          )}
+          <div className="tool-container">{tool}</div>
         </div>
-    </>;
+        <footer className="toolbox-footer">
+          <div className="toolbox-footer-text">
+            All tools are running locally in your browser: no data is sent to
+            the server &nbsp;|&nbsp; Made by{" "}
+            <a href="https://valerian.dtdg.fr">Valerian de Thézan de Gaussan</a>{" "}
+            &nbsp;|&nbsp; Code is open-sourced{" "}
+            <a href="https://github.com/vdtdg/dev-toolbox">here</a>,
+            contributions are welcomed!
+          </div>
+        </footer>
+      </div>
+    </>
+  );
 }
