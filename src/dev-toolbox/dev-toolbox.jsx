9481d5e176b1c80460a4aba9bf10d638df664f4d
hamzakhankakar <40837238+hamzakhankakar@users.noreply.github.com>
2024-04-28 21:10:00 +0500

    Update dev-toolbox.jsx
    footor added

--- src/dev-toolbox/dev-toolbox.jsx
+++ src/dev-toolbox/dev-toolbox.jsx
@@ -39,11 +39,11 @@ export default function DevToolBox() {
           </div>
         </div>
         <footer className="box toolbox-footer">
-          {/* <p className="toolbox-footer-text">
+          <p className="toolbox-footer-text">
             All tools are running locally in your browser: no data is sent to the server &nbsp;|&nbsp; Made by <a href="https://valerian.dtdg.fr">Valérian
             de Thézan de Gaussan</a> &nbsp;|&nbsp; Code is open-sourced <a href="https://github.com/vdtdg/dev-toolbox">here</a>, contributions are
             welcomed!
-          </p> */}
+          </p> 
         </footer>
       </div>
     </>
