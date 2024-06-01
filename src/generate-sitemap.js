80babbb751cb222db0ff58f709a8d2e3e9c3882b
vdtdg <valerian@dtdg.fr>
2024-06-01 20:48:09 +0200

    Script to generate a sitemap. Added it to the build instructions so that a fresh sitemap is generated before any build.

---
+++ src/generate-sitemap.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+const seoData = require("./dev-toolbox/seo-data");
+require("dotenv").config();
+
+const baseUrl = new URL(process.env.BASE_PATH, process.env.PUBLIC_URL).href;
+
+function generateSitemap(toolList, baseUrl) {
+  const urls = Object.entries(toolList).map(([toolName, toolData]) => {
+    return `<url>
+      <loc>${baseUrl}/${toolData.path}</loc>
+    </url>`;
+  });
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls.join("")}
+  </urlset>`;
+
+  return sitemap;
+}
+
+const sitemap = generateSitemap(seoData, baseUrl);
+
+fs.writeFileSync(path.join(__dirname, "../public", "sitemap.xml"), sitemap);
+
+console.log("sitemap.xml generated successfully");
