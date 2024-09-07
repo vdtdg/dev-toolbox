8cc21d2869d59de13b495aabc86345216e495d7c
Valérian de Thézan de Gaussan <valerian@dtdg.fr>
2024-09-07 17:00:07 +0200

    Removed seo warning + Upgraded the title

--- src/dev-toolbox/seo.jsx
+++ src/dev-toolbox/seo.jsx
@@ -12,16 +12,16 @@ export default function SeoWrapper({ children, name, path }) {
       <>
         <Helmet>
           <meta charSet="utf-8" />
-          <title>{name} - Access Top Developer Tools Online</title>
+          <title>{name} - Access Top Developer Tools Online 100% Free</title>
           <link
             rel="canonical"
             href={consts.host + consts.basePath + "/" + path}
           />
           {data.metaTags.map((tag) => (
-            <meta name={tag.name} content={tag.content} />
+            <meta key={tag.name} name={tag.name} content={tag.content} />
           ))}
           {data.ogTags.map((tag) => (
-            <meta property={tag.property} content={tag.content} />
+            <meta key={tag.property} property={tag.property} content={tag.content} />
           ))}
         </Helmet>
         {children}
